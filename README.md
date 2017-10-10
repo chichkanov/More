a49b1f76c94747b827c7956cab9af1404119383e
chichkanov <agchichkanov@gmail.com>
2017-10-11 00:09:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# More
+Android reminder application
---
+++ app/src/main/java/com/chichkanov/more/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.chichkanov.more
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
